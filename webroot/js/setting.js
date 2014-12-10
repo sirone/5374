ce75f5600a69fa131d4b8bd799e326203583d1d1
柳生広樹 <hiroki.yagyu@nijibox.co.jp>
2014-12-10 23:51:40 +0900

    initial commit.

---
+++ webroot/js/setting.js
@@ -0,0 +1,12 @@
+/*
+ * 5374 setting
+ *
+*/
+
+var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+
+var MaxDescription = 11; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+
+var MaxMonth = 6;
+
+var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
